e2287353adbff640eaec5a2ce243d9e6960cc58f
Nikhil Gupta <guptanikhil801@gmail.com>
2023-12-10 23:47:40 +0530

    Linq programs added

--- CsharpConcepts/Classes/LINQPracticeSet.cs
+++ CsharpConcepts/Classes/LINQPracticeSet.cs
@@ -1,4 +1,6 @@
 ﻿
+using static System.Net.Mime.MediaTypeNames;
+
 namespace CsharpConcepts.Classes;
 public class LINQPracticeSet
 {
@@ -9,10 +11,40 @@ public class LINQPracticeSet
         return evenNumbers.ToArray();
     }
 
+    /// <summary>
+    /// Write a program in C# Sharp to find the +ve numbers from a list of numbers using two where
+    /// conditions in LINQ Query
+    /// </summary>
+    /// <returns>Integer Array having only even numbers</returns>
     public static int[] GetEvenNumbersInARangeUsingTwoWhere(int[] numbers, int startRange, int endRange)
     {
-        var evenNumbers = from number in numbers where (number % 2 == 0) && number >= startRange && number < endRange  select number;  //Using query Syntax
+        var evenNumbers = from number in numbers where (number % 2 == 0) 
+                          && number >= startRange && number < endRange  select number;  //Using query Syntax
         return evenNumbers.ToArray();
+    }
+
+    /// <summary>
+    /// find the number of an array and the square of each number using Linq
+    /// </summary>
+    /// <param name="numbers"></param>
+    /// <returns></returns>
+    public static IEnumerable<dynamic> FindNumberAndReturnSquare(int[] numbers)
+    {
+        var result = from num in numbers let square = num*num select new { num, square };
+        return result;
+    }
+
+    /// <summary>
+    /// find a string that starts and ends with a specific character.
+    /// </summary>
+    /// <returns></returns>
+    public static IEnumerable<string> FindStringThatStartsAndEndsWithSpecificCharacter(string[] WordCollection, char start, char end)
+    {
+        var result = from collection in WordCollection
+                     where collection.StartsWith(start) &&
+                     collection.EndsWith(end)
+                     select collection;
+        return result;
     }
 }
 
