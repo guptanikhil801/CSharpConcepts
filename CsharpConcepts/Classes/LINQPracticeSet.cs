674db50e500e7c9c87e37cf78b378825da76021c
Nikhil Gupta <guptanikhil801@gmail.com>
2023-12-12 21:29:03 +0530

    programs related to Linq methods added

--- CsharpConcepts/Classes/LINQPracticeSet.cs
+++ CsharpConcepts/Classes/LINQPracticeSet.cs
@@ -9,7 +9,7 @@ public class LINQPracticeSet
     public static int[] GetEvenNumbers(int[] numbers)
     {
         // var evenNumbers = numbers.Where(x => x % 2 == 0).ToArray();  Using Method Syntax
-        var evenNumbers = from number in numbers where (number%2 == 0) select number;  //Using query Syntax
+        var evenNumbers = from number in numbers where (number % 2 == 0) select number;  //Using query Syntax
         return evenNumbers.ToArray();
     }
 
@@ -20,8 +20,10 @@ public class LINQPracticeSet
     /// <returns>Integer Array having only even numbers</returns>
     public static int[] GetEvenNumbersInARangeUsingTwoWhere(int[] numbers, int startRange, int endRange)
     {
-        var evenNumbers = from number in numbers where (number % 2 == 0) 
-                          && number >= startRange && number < endRange  select number;  //Using query Syntax
+        var evenNumbers = from number in numbers
+                          where (number % 2 == 0)
+                          && number >= startRange && number < endRange
+                          select number;  //Using query Syntax
         return evenNumbers.ToArray();
     }
 
@@ -32,7 +34,7 @@ public class LINQPracticeSet
     /// <returns></returns>
     public static IEnumerable<dynamic> FindNumberAndReturnSquare(int[] numbers)
     {
-        var result = from num in numbers let square = num*num select new { num, square };
+        var result = from num in numbers let square = num * num select new { num, square };
         return result;
     }
 
@@ -40,7 +42,7 @@ public class LINQPracticeSet
     /// find a string that starts and ends with a specific character.
     /// </summary>
     /// <returns></returns>
-    public static IEnumerable<string> FindStringThatStartsAndEndsWithSpecificCharacter(string[] WordCollection, char start, char end)
+    public static IEnumerable<string> StringThatStartsAndEndsWithSpecificCharacter(string[] WordCollection, char start, char end)
     {
         var result = from collection in WordCollection
                      where collection.StartsWith(start) &&
@@ -49,12 +51,18 @@ public class LINQPracticeSet
         return result;
     }
 
+    public static IEnumerable<string> StringThatStartsAndEndsWithSpecificCharacterMethodSyntax(string[] WordCollection, char start, char end)
+    {
+        var result = WordCollection.Where( x=> x.StartsWith(start) && x.EndsWith(end));
+        return result;
+    }
+
     /// <summary>
     /// display the number and frequency of a given number from an array
     /// </summary>
     /// <param name="numbers">list of numbers</param>
     /// <returns>number with frequency</returns>
-    public static Dictionary<int,int> FrequencyOfGivenNumbers(int[] numbers)
+    public static Dictionary<int, int> FrequencyOfGivenNumbers(int[] numbers)
     {
         Dictionary<int, int> numberWithFrequency = new Dictionary<int, int>();
         var result = from number in numbers group number by number into y select y;
@@ -81,11 +89,31 @@ public class LINQPracticeSet
         return charWithFrequency;
     }
 
+    public static Dictionary<char, int> FrequencyOfEachCharactersMethodSyntax(string word)
+    {
+        Dictionary<char, int> charWithFrequency = new Dictionary<char, int>();
+        var result = word.GroupBy(x => x).ToDictionary( group => group.Key, group => group.Count());
+        return result;
+    }
+
     public static IEnumerable<dynamic> NumberWithFrequencyAndMultiplication(int[] numbers)
     {
         var result = from number in numbers
                      group number by number into data
                      select new { Number = data.Key, Frequency = data.Count(), Multiplication = data.Key * data.Count() };
+        return result;
+    }
+
+    public static IEnumerable<dynamic> NumberWithFrequencyAndMultiplicationUsingMethods(int[] numbers)
+    {
+        var result = numbers
+            .GroupBy(number => number)
+            .Select(data => new
+            {
+                Number = data.Key,
+                Frequency = data.Count(),
+                Multiplication = data.Key * data.Count()
+            }); ;
         return result;
     }
 }
