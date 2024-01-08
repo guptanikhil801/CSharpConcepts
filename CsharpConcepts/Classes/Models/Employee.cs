8bc62d04e4da1cce41e33146ff40bd91020ddc73
Nikhil Gupta <guptanikhil801@gmail.com>
2024-01-08 17:17:47 +0530

    Encapsulation usage added

--- CsharpConcepts/Classes/Models/Employee.cs
+++ CsharpConcepts/Classes/Models/Employee.cs
@@ -5,7 +5,15 @@
         public int EmployeeId { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public int Salary { get; set; }
+        //public int Salary { get; set; }
+        private int EmployeeSalary;
+
+        public int Salary
+        {
+            get => EmployeeSalary;
+            set { EmployeeSalary = value < 10000 ? 10000 : value; } // encapsulation example
+        }
+
         public string[]? SkillSet { get; set; }
     }
 }
