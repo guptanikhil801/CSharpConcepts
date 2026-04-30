6dad9ba74bb6a5b46b308bd8b4759e7695d52b19
Nikhil Gupta <guptanikhil801@gmail.com>
2026-04-30 18:22:46 +0530

    program.cs added to gitignore

--- CsharpConcepts/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using CsharpConcepts;
-using CsharpConcepts.Classes;
-
-var evenNumbers = LINQPracticeSet.GetEvenNumbersInARangeUsingTwoWhere(new int[] { 67, 9, 70, 6, 7 },1,10);
-for (int i = 0; i < evenNumbers.Length; i++)
-{
-    Console.WriteLine(evenNumbers[i]);
-}
-Console.ReadKey();
